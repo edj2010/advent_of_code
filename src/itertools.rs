03467ee2e473c7b8122f0fca40ea816aa5541cdd
Evan Johnson <evan.durston.johnson@gmail.com>
2023-12-09 13:07:57 -0500

    added contains

--- src/itertools.rs
+++ src/itertools.rs
@@ -9,6 +9,13 @@ pub trait Itertools: Iterator + Sized {
         self.for_each(|item| *(counts.entry(item).or_default()) += 1);
         counts
     }
+
+    fn contains(mut self, item: &Self::Item) -> bool
+    where
+        Self::Item: PartialEq,
+    {
+        self.find(|i| i == item).is_some()
+    }
 }
 
 impl<T> Itertools for T where T: Iterator {}
